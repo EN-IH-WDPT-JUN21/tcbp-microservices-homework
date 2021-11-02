33e6f9b06e4ca6a6fafb6cf9cdb056cf0bb2464c
vivi-chaudiem <nguyen.chaudiem@yahoo.de>
2021-11-02 23:37:07 +0100

    Added routes for reporting

--- account-service/src/main/java/com/chocolateboxproject/accountservice/service/IAccountService.java
+++ account-service/src/main/java/com/chocolateboxproject/accountservice/service/IAccountService.java
@@ -2,6 +2,7 @@ package com.chocolateboxproject.accountservice.service;
 
 import com.chocolateboxproject.accountservice.dao.Account;
 import com.chocolateboxproject.accountservice.dto.AccountDTO;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +16,24 @@ public interface IAccountService {
     Account createNewAccount(AccountDTO accountDTO);
 
     Account updateAccount(Long id, AccountDTO accountDTO);
+
+    Double getMeanEmployeeCount();
+
+    Integer getMaxEmployeeCount();
+
+    Integer getMinEmployeeCount();
+
+    List<Integer> getEmployeeCountList();
+
+    long countOpportunitiesByCountry(String country);
+
+    long countOpportunitiesByCountryStatus(String country, String status);
+
+    long countOpportunitiesByCity(String city);
+
+    long countOpportunitiesByCityStatus(String city, String status);
+
+    long countOpportunitiesByIndustry(String industry);
+
+    long countOpportunitiesByIndustryStatus(String country, String status);
 }
--- opportunity-service/src/main/java/com/ironhack/opportunityservice/interfaces/IOpportunityService.java
+++ opportunity-service/src/main/java/com/ironhack/opportunityservice/interfaces/IOpportunityService.java
@@ -2,10 +2,27 @@ package com.ironhack.opportunityservice.interfaces;
 
 import com.ironhack.opportunityservice.dao.Opportunity;
 import com.ironhack.opportunityservice.dto.OpportunityDTO;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IOpportunityService {
     Opportunity store(OpportunityDTO opportunityDTO);
     Opportunity updateStatusClosedLost(String id);
     Opportunity updateStatusClosedWon(String id);
+
+    Double getMeanOpportunityCountPerAccount();
+    Integer getMaxOpportunityCountPerAccount();
+    Integer getMinOpportunityCountPerAccount();
+    List<Integer> getListOpportunityCountPerAccount();
+    Double getMeanProductQuantity();
+    Integer getMaxProductQuantity();
+    Integer getMinProductQuantity();
+    List<Integer> getProductQuantityList();
+
+    long countOpportunitiesBySalesRep(String name);
+    long countOpportunitiesBySalesRepAndStatus(String name, String status);
+    long countOpportunitiesByProduct(String product);
+    long countOpportunitiesByProductStatus(String product, String status);
 
 }
